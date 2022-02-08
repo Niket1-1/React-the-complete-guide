426d58dee2dbfdddc61000963af67ac51ec163f2
Niket Nayan <niketnayan13@gmail.com>
2022-02-09 00:09:06 +0530

    Error Modellling start

--- completeproject/src/App.js
+++ completeproject/src/App.js
@@ -2,11 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import AddUser from './Componenets/Users/AddUsers';
 import UsersList from './Componenets/Users/UseraList';
+import { useState } from 'react';
 function App() {
+
+  const [userslist,setuserlist]=useState([]);
+
+
+  const addUserHnadler=(uname,uage)=>{
+    setuserlist((prevuserlist)=>{
+      return [...prevuserlist,{name:uname,age:uage,id:Math.random().toString()}];
+    })
+  }
   return (
     <div>
-    <AddUser />
-    <UsersList users={[]} />
+    <AddUser  onAddUser={addUserHnadler}/>
+    <UsersList users={userslist} />
     </div>
   );
 }
--- completeproject/src/Componenets/Users/UseraList.jsx
+++ completeproject/src/Componenets/Users/UseraList.jsx
@@ -6,7 +6,7 @@ const UsersList =(props)=>{
     return (
     <Card className={classes.users}>
     <ul>
-        {props.users.map(user=><li>{user.name} ({user.age}years old)</li>)}
+        {props.users.map((user,index)=>(<li key={user.id}>{user.name} ({user.age}years old) </li>))}
     </ul>
     </Card>
     )}
