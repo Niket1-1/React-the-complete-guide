5905c24e98235942e7e73ba89ca6c836e1b5f5c2
Niket Nayan <niketnayan13@gmail.com>
2022-02-21 10:26:35 +0530

    adding form handler

--- formsuserinput/src/components/SimpleInput.jsx
+++ formsuserinput/src/components/SimpleInput.jsx
@@ -1,9 +1,27 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+   const [Enteredname,setEnterdename]=useState("")
+   const enterValue=useRef()
+   const nameInputChangeHandler=(e)=>{
+       const val=e.target.value;
+       setEnterdename(val)
+   }
+   const formSubmissionHandler=(e)=>{
+       e.preventDefault()
+          
+       console.log(Enteredname)
+          // const va=enterValue.current.value;
+         
+       setEnterdename("")
+   }
+
     return (
-      <form>
+      <form onSubmit={formSubmissionHandler}>
         <div className='form-control'>
           <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
+          <input ref={enterValue} type='text' id='name' onChange={nameInputChangeHandler}/>
         </div>
         <div className="form-actions">
           <button>Submit</button>
